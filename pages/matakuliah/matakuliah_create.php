02eb28b5e7cf53737cb4143717c6b23cd7ffe226
mirzayogy <mirza.yogy@gmail.com>
2020-06-14 10:20:45 +0800

    modal header

--- pages/matakuliah/matakuliah_create.php
+++ pages/matakuliah/matakuliah_create.php
@@ -3,7 +3,10 @@
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
-
+          <h5 class="modal-title" id="anggotaModalLabel">Create</h5>
+          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
         </div>
         <div class="modal-footer">
           <button class="btn btn-success" name="create" type="submit" >Simpan</button>
